419dbcec23f7635dcd1edc8245610780903cfd0f
Kacper Ochotny <trinerxd@gmail.com>
2022-04-28 22:03:28 +0200

    Models moved to separate repo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Models
+
+Creating Linux-esque soft link to ***directory only*** (admin cmd)
+```
+admin-cmd> mklink /J <target> <path\to\directory>
+
+admin-cmd> mklink /J Models ..\Models
+```
+
+Second line can be used for each service directory *if* you're using the same project structure as described in Docker README
---
+++ Transport/ReserveTransportEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Models.Transport
+{
+    public class ReserveTransportEvent : EventModel
+    {
+        // new data to be transfered/saved to DB
+        // add new data fields + a contstructor for them
+
+        // no need to add the [JsonProperty] here according to MS Documentation/Source Code
+        public int DestId { get; set; } // some transfarable data
+
+        public ReserveTransportEvent(int DestId) : base() // be wary of the base() call
+        {
+            this.DestId = DestId;
+        }
+    }
+}

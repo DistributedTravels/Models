4c79e324557fa8141bef106a492000ddaaf80006
Mateusz <mateusz.niescier@onet.pl>
2022-05-07 15:35:39 +0200

    first models for Hotels
    Signed-off-by: Mateusz <mateusz.niescier@onet.pl>

---
+++ Hotels/GetHotelsEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Models.Hotels
+{
+    public class GetHotelsEvent : EventModel
+    {
+        public String Country { get; set; }
+        public String Attraction { get; set; }
+
+        public GetHotelsEvent(string country, string attraction) : base()
+        {
+            Country = country;
+            Attraction = attraction;
+        }
+    }
+}

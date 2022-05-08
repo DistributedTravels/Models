007a336b190d11c3b3e9f81de76916cc015c0fbf
Kacper Ochotny <trinerxd@gmail.com>
2022-05-08 18:26:15 +0200

    Payments models

---
+++ Payments/ProcessPaymentEvent.cs
@@ -0,0 +1,22 @@
+﻿namespace Models.Payments
+{
+    public class ProcessPaymentEvent : EventModel
+    {
+        public CardCredentials Card { get; set; }
+        public double Price { get; set; }
+
+        public ProcessPaymentEvent(CardCredentials card, double price) : base()
+        {
+            Card = card;
+            Price = price;
+        }
+    }
+
+    public class CardCredentials
+    {
+        public string Number { get; set; }
+        public int CVV { get; set; }
+        public string ExpDate { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ Payments/ProcessPaymentReplyEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace Models.Payments
+{
+    public class ProcessPaymentReplyEvent : EventModel
+    {
+        public enum State 
+        { 
+            ACCEPTED,
+            INVALID_CARD_CREDENTIALS,
+            LIMITS_TOO_LOW
+        }
+        public State Response { get; set; }
+
+        public ProcessPaymentReplyEvent(State resp, Guid cId) : base()
+        {
+            Response = resp;
+            CorrelationId = cId;
+        }
+    }
+}

527a41935f30b1c7b9e50fb8619e79936f9de517
Mateusz <mateusz.niescier@onet.pl>
2022-05-09 11:19:55 +0200

    new Hotel models
    Signed-off-by: Mateusz <mateusz.niescier@onet.pl>

--- Hotels/GetHotelsEvent.cs
+++ Hotels/GetHotelsEvent.cs
@@ -3,12 +3,30 @@
     public class GetHotelsEvent : EventModel
     {
         public String Country { get; set; }
-        public String Attraction { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int AppartmentsAmount { get; set; }
+        public int CasualRoomAmount { get; set; }
+        public Boolean Breakfast { get; set; }
+        public Boolean Internet { get; set; }
 
-        public GetHotelsEvent(string country, string attraction) : base()
+
+        public GetHotelsEvent(
+            string country,
+            DateTime beginDate,
+            DateTime endDate,
+            int appartmentsAmount,
+            int casualRoomAmount,
+            Boolean breakfast,
+            Boolean internet) : base()
         {
             Country = country;
-            Attraction = attraction;
+            BeginDate = beginDate;
+            EndDate = endDate;
+            AppartmentsAmount = appartmentsAmount;
+            CasualRoomAmount = casualRoomAmount;
+            Breakfast = breakfast;
+            Internet = internet;
         }
     }
 }
---
+++ Hotels/ReserveRoomsEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace Models.Hotels
+{
+    public class ReserveRoomsEvent : EventModel
+    {
+        public String HotelName { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int AppartmentsAmount { get; set; }
+        public int CasualRoomAmount { get; set; }
+
+        public ReserveRoomsEvent(
+            string hotelName, DateTime beginDate, DateTime endDate,
+            int appartmentsAmount, int casualRoomAmount,
+            Boolean breakfast, Boolean internet
+        ) : base()
+        {
+            HotelName = hotelName;
+            BeginDate = beginDate;
+            EndDate = endDate;
+            AppartmentsAmount = appartmentsAmount;
+            CasualRoomAmount = casualRoomAmount;
+        }
+    }
+}

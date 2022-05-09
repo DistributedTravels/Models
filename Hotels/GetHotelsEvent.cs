be1453d6fa342403815d118b80e1049522d21ca2
Mateusz Nieścier <mateusz.niescier@onet.pl>
2022-05-10 00:49:22 +0200

    hotel models reorganization
    Signed-off-by: Mateusz <mateusz.niescier@onet.pl>

--- Hotels/GetHotelsEvent.cs
+++ Hotels/GetHotelsEvent.cs
@@ -3,30 +3,12 @@
     public class GetHotelsEvent : EventModel
     {
         public String Country { get; set; }
-        public DateTime BeginDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public int AppartmentsAmount { get; set; }
-        public int CasualRoomAmount { get; set; }
-        public Boolean Breakfast { get; set; }
-        public Boolean Internet { get; set; }
 
 
         public GetHotelsEvent(
-            string country,
-            DateTime beginDate,
-            DateTime endDate,
-            int appartmentsAmount,
-            int casualRoomAmount,
-            Boolean breakfast,
-            Boolean internet) : base()
+            string country) : base()
         {
             Country = country;
-            BeginDate = beginDate;
-            EndDate = endDate;
-            AppartmentsAmount = appartmentsAmount;
-            CasualRoomAmount = casualRoomAmount;
-            Breakfast = breakfast;
-            Internet = internet;
         }
     }
 }

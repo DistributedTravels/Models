1cee9cb94eeb0b7d66c8cf1a91e61e9c09798fb3
Mateusz <mateusz.niescier@onet.pl>
2022-05-09 13:27:18 +0200

    room reservation models
    Signed-off-by: Mateusz <mateusz.niescier@onet.pl>

--- Hotels/ReserveRoomsEvent.cs
+++ Hotels/ReserveRoomsEvent.cs
@@ -2,24 +2,25 @@
 {
     public class ReserveRoomsEvent : EventModel
     {
-        public String HotelName { get; set; }
+        public int HotelId { get; set; }
         public DateTime BeginDate { get; set; }
         public DateTime EndDate { get; set; }
 
         public int AppartmentsAmount { get; set; }
         public int CasualRoomAmount { get; set; }
+        public Guid UserId { get; set; }
 
         public ReserveRoomsEvent(
-            string hotelName, DateTime beginDate, DateTime endDate,
+            int hotelId, DateTime beginDate, DateTime endDate,
             int appartmentsAmount, int casualRoomAmount,
-            Boolean breakfast, Boolean internet
-        ) : base()
+            Guid userId) : base()
         {
-            HotelName = hotelName;
+            HotelId = hotelId;
             BeginDate = beginDate;
             EndDate = endDate;
             AppartmentsAmount = appartmentsAmount;
             CasualRoomAmount = casualRoomAmount;
+            UserId = userId;
         }
     }
 }
---
+++ Hotels/ReserveRoomsEventReply.cs
@@ -0,0 +1,19 @@
+﻿namespace Models.Hotels
+{
+    public class ReserveRoomsEventReply : EventModel
+    {
+        public enum State
+        {
+            RESERVED,
+            NOT_RESERVED
+        };
+
+        public State Answer { get; set; }
+
+        public ReserveRoomsEventReply(State ans, Guid cId) : base()
+        {
+            Answer = ans;
+            CorrelationId = cId;
+        }
+    }
+}

45fe26a40bc6fe0fadfd49083d1e9c6054a62c3c
Mateusz <mateusz.niescier@onet.pl>
2022-05-12 23:41:33 +0200

    new models for GetInfoFromHotelEvent
    Signed-off-by: Mateusz <mateusz.niescier@onet.pl>

---
+++ Hotels/GetInfoFromHotelEvent.cs
@@ -0,0 +1,27 @@
+﻿namespace Models.Hotels
+{
+    public class GetInfoFromHotelEvent : EventModel
+    {
+        public int HotelId { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int AppartmentsAmount { get; set; }
+        public int CasualRoomAmount { get; set; }
+        public Boolean Breakfast { get; set; }
+        public Boolean Wifi { get; set; }
+
+        public GetInfoFromHotelEvent(
+            int hotelId, DateTime beginDate, DateTime endDate,
+            int appartmentsAmount, int casualRoomAmount,
+            Boolean breakfast, Boolean wifi) : base()
+        {
+            HotelId = hotelId;
+            BeginDate = beginDate;
+            EndDate = endDate;
+            AppartmentsAmount = appartmentsAmount;
+            CasualRoomAmount = casualRoomAmount;
+            Breakfast = breakfast;
+            Wifi = wifi;
+        }
+    }
+}
---
+++ Hotels/GetInfoFromHotelEventReply.cs
@@ -0,0 +1,21 @@
+﻿namespace Models.Hotels
+{
+    public class GetInfoFromHotelEventReply : EventModel
+    {
+        public enum State
+        {
+            CAN_BE_RESERVED,
+            CAN_NOT_BE_RESERVED
+        };
+
+        public State Answer { get; set; }
+        public double Price { get; set; }
+
+        public GetInfoFromHotelEventReply(State ans, double price, Guid cId) : base()
+        {
+            Answer = ans;
+            Price = price;
+            CorrelationId = cId;
+        }
+    }
+}

326b5521ece6a4fd78dab499f172e3e1ea910942
jakub_czarnecki <jakub.piotr.czarnecki@gmail.com>
2022-05-13 20:49:14 +0200

    event for adding offers to database

---
+++ Offers/SaveOffersToDatabaseEvent.cs
@@ -0,0 +1,8 @@
+﻿using Models.Offers.Dto;
+namespace Models.Offers
+{
+    public class SaveOffersToDatabaseEvent : EventModel
+    {
+        public IEnumerable<TripDto> Trips { get; set; }
+    }
+}

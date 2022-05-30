b72bfb8596c871af4b8f9153883517de486d9b67
jakub_czarnecki <jakub.piotr.czarnecki@gmail.com>
2022-05-30 21:10:18 +0200

    new event

---
+++ Reservations/NewReservationSuccessfullyBookedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Models.Reservations
+{
+    public class NewReservationSuccessfullyBookedEvent : EventModel
+    {
+        public string Destination { get; set; }
+        public string HotelName { get; set; }
+        public string User { get; set; }
+    }
+}

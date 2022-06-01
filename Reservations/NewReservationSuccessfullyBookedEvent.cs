5671a7be7b5b1e64d7da8676bad580686ffb892d
jakub_czarnecki <jakub.piotr.czarnecki@gmail.com>
2022-06-01 20:04:22 +0200

    new reservation event update

--- Reservations/NewReservationSuccessfullyBookedEvent.cs
+++ Reservations/NewReservationSuccessfullyBookedEvent.cs
@@ -5,5 +5,8 @@
         public string Destination { get; set; }
         public string HotelName { get; set; }
         public string User { get; set; }
+        public int SmallRooms { get; set; }
+        public int BigRooms { get; set; }
+        public bool HasOwnTransport { get; set; }
     }
 }

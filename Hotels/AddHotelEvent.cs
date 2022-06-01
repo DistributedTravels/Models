3cfdaf7365841c75bd1a9ba6f9f339e8f72fe658
Mateusz <mateusz.niescier@onet.pl>
2022-06-01 21:46:13 +0200

    add hotels event model
    Signed-off-by: Mateusz <mateusz.niescier@onet.pl>

---
+++ Hotels/AddHotelEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Models.Hotels
+{
+    public class AddHotelEvent : EventModel
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        //potetially could be more conveniences
+        public double BreakfastPrice { get; set; }
+        public bool HasWifi { get; set; }
+        public double PriceForNightForPerson { get; set; }
+        public int AppartmentsAmount { get; set; }
+        public int CasualRoomAmount { get; set; }
+    }
+}
---
+++ Hotels/AddHotelEventReply.cs
@@ -0,0 +1,18 @@
+﻿namespace Models.Hotels
+{
+    public class AddHotelEventReply : EventModel
+    {
+        public enum State
+        {
+            ADDED,
+            NOT_ADDED
+        };
+        public State Answer;
+
+        public AddHotelEventReply(State answer, Guid cId)
+        {
+            Answer = answer;
+            CorrelationId = cId;
+        }
+    }
+}

c5e4c97acdfbae4d332566d78b5c889322b5ffeb
Mateusz <mateusz.niescier@onet.pl>
2022-06-02 20:56:14 +0200

    models for price changing in hotels
    Signed-off-by: Mateusz <mateusz.niescier@onet.pl>

---
+++ Hotels/ChangeBasePriceEventReply.cs
@@ -0,0 +1,22 @@
+﻿using Models.Hotels.Dto;
+
+namespace Models.Hotels
+{
+    public class ChangeBasePriceEventReply : EventModel
+    {
+        public enum State
+        {
+            CHANGED,
+            NOT_CHANGED
+        };
+        public State Answer;
+        public IEnumerable<ResponseListDto>? ResponseListDto { get; set; }
+
+        public ChangeBasePriceEventReply(State answer, IEnumerable<ResponseListDto>? responseListDto, Guid cId)
+        {
+            Answer = answer;
+            ResponseListDto = responseListDto;
+            CorrelationId = cId;
+        }
+    }
+}

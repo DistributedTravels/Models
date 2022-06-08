e4e0e99a328a9a7119064cb225899386ccfced20
Kacper Ochotny <trinerxd@gmail.com>
2022-06-08 21:39:44 +0200

    dto update

--- Transport/Dto/TravelChangeDto.cs
+++ Transport/Dto/TravelChangeDto.cs
@@ -3,8 +3,9 @@
     public class TravelChangeDto
     {
         public int Id { get; set; } = -1;
-        public string Source { get; set; } = "";
-        public string Destination { get; set; } = "";
+        public int Source { get; set; } = -1;
+        public int Destination { get; set; } = -1;
+        public bool Direction { get; set; } = false;
         public DateTime DepartureTime { get; set; } = DateTime.UnixEpoch;
         public int AvailableSeats { get; set; } = -1;
         public double Price { get; set; } = -1;

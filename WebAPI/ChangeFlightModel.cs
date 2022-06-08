09cf0683cb3310005375186ba4f893861e56dc4b
jakub_czarnecki <jakub.piotr.czarnecki@gmail.com>
2022-06-08 21:39:37 +0200

    changing hotels and travels for testing

---
+++ WebAPI/ChangeFlightModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Models.WebAPI
+{
+    public class ChangeFlightModel
+    {
+        public int Id { get; set; }
+        public int FreeSeats { get; set; }
+    }
+}
---
+++ WebAPI/ChangeHotelModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Models.WebAPI
+{
+    public class ChangeHotelModel
+    {
+        public int Id { get; set; }
+        public double NewPrice { get; set; }
+    }
+}
